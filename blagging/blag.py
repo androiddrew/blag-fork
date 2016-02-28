b17313ce852182cfa57b00cf749415288547d789
androiddrew <bednar.andrew@gmail.com>
2016-02-27 19:30:09 -0500

    Adding blog template

--- blagging/blag.py
+++ blagging/blag.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['DEBUG']= True
+app.config['DEBUG'] = True
+
 
 @app.route("/")
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('blog.html')
 
 if __name__ == "__main__":
     app.run()
-    
