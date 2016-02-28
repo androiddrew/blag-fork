4ab9f5d4437b1ad5133b12dcd761565a597056ca
androiddrew <bednar.andrew@gmail.com>
2016-02-28 09:03:58 -0500

    adding error handlers

--- blagging/views.py
+++ blagging/views.py
@@ -3,8 +3,18 @@ from flask import render_template
 from . import app, db
 from .models import Post
 
+
 @app.route("/")
 @app.route('/index')
 def index():
     return render_template('blog.html', new_posts=Post.newest(5))
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500 # Debug needs to be turned off to hit a 500
