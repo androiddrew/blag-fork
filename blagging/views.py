ff49077b71f1401266dabc61a5e474a139961af2
androiddrew <bednar.andrew@gmail.com>
2016-02-28 19:36:13 -0500

    Refactored views and models, added post template

--- blagging/views.py
+++ blagging/views.py
@@ -4,11 +4,21 @@ from . import app, db
 from .models import Post
 
 
-@app.route("/")
+@app.route('/')
 @app.route('/index')
 def index():
     return render_template('blog.html', new_posts=Post.newest(5))
 
+"""
+@app.route('/post/<int:postid>')
+def post(postid):
+    post = Post.query.filter_by(id=postid).first_or_404()
+    return render_template('post.html', post=post)
+"""
+@app.route('/post/<slug>')
+def post(slug):
+    post = Post.query.filter_by(display_title=slug).first_or_404()
+    return render_template('post.html', post=post)
 
 @app.errorhandler(404)
 def page_not_found(e):
