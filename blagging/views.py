e1b11ed3485601de20d537cc6cda08d80d606792
androiddrew <2Programming$>
2016-12-21 12:06:50 -0500

    Changed post list layout and added jinja filter for dates

--- blagging/views.py
+++ blagging/views.py
@@ -6,7 +6,7 @@ from .models import Post, Tag, Author, tags as Post_Tag, Comment
 from .forms import LoginForm, PostForm, CommentForm
 
 
-#Auth#################
+# Auth#################
 @login_manager.user_loader
 def load_user(userid):
     return Author.query.get(int(userid))
@@ -29,13 +29,13 @@ def logout():
     return redirect(url_for('index'))
 
 
-#MAIN##############
+# MAIN##############
 
 @app.route('/')
 @app.route('/blog')
 @app.route('/blog/page/<int:page_num>')
 def index(page_num=1):
-    query = Post.query.filter(Post.published==True)
+    query = Post.query.filter(Post.published == True)
     pagination = query.order_by(Post.date.desc()).paginate(page=page_num, per_page=app.config['POST_PER_PAGE'],
                                                            error_out=True)
     return render_template('blog.html', pagination=pagination, authors=Author.query.all())
@@ -43,7 +43,7 @@ def index(page_num=1):
 
 @app.route('/post/<slug>', methods=['GET', 'POST'])
 def post(slug):
-    post = Post.query.filter_by(_display_title=slug).filter(Post.published==True).first_or_404()
+    post = Post.query.filter_by(_display_title=slug).filter(Post.published == True).first_or_404()
     form = CommentForm()
     if form.validate_on_submit():
         comment_text = form.text.data
@@ -59,9 +59,11 @@ def post(slug):
 @app.route('/tag/<name>/<int:page_num>')
 def tag(name, page_num=1):
     tag = Tag.query.filter_by(name=name).first_or_404()
-    query = Post.query.join(Post_Tag).join(Tag).filter(Tag.id == tag.id).filter(Post.published==True)
-    pagination = query.filter(Post.published==True).order_by(Post.date.desc()).paginate(page=page_num, per_page=app.config['POST_PER_PAGE'],
-                                                           error_out=True)
+    query = Post.query.join(Post_Tag).join(Tag).filter(Tag.id == tag.id).filter(Post.published == True)
+    pagination = query.filter(Post.published == True).order_by(Post.date.desc()).paginate(page=page_num,
+                                                                                          per_page=app.config[
+                                                                                              'POST_PER_PAGE'],
+                                                                                          error_out=True)
     return render_template('tag.html', pagination=pagination, tag=tag)
 
 
@@ -82,7 +84,7 @@ def add():
         tags = form.tags.data
         published = int(form.published.data)
         post = Post(author=current_user, title=title, display_title=title, short_desc=short_desc, body=body, tags=tags,
-                     published=published)
+                    published=published)
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('index'))
@@ -110,7 +112,6 @@ def edit_post(post_id):
     return render_template('post_form.html', form=form)
 
 
-
 @app.route('/preview', methods=['GET', 'POST'])
 @login_required
 def preview_post():
@@ -120,16 +121,17 @@ def preview_post():
     form_data['author'] = current_user
     for field in result:
         form_data[field['name']] = field['value']
-    form_data['tags']= form_data.get('tags').split(',')
+    form_data['tags'] = form_data.get('tags').split(',')
     return render_template('post_preview.html', post=form_data)
 
-#MAIN OTHER###########
+
+# MAIN OTHER###########
 @app.errorhandler(403)
 def page_not_found(e):
     return render_template('403.html'), 403
 
 
-@app.errorhandler(404) # bluprintname.app_errorhandler will register for the entire app when using blueprints
+@app.errorhandler(404)  # bluprintname.app_errorhandler will register for the entire app when using blueprints
 def page_not_found(e):
     return render_template('404.html'), 404
 
@@ -144,6 +146,14 @@ def inject_tags():
     """context_processor similar to the app_context_processor for blueprints"""
     return dict(all_tags=Tag.all)
 
+
 @app.context_processor
 def inject_auth_url():
     return dict(auth_url=login_url)
+
+
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date, fmt=None):
+    if fmt is None:
+        fmt = '%Y-%m-%d'
+    return date.strftime(fmt)
